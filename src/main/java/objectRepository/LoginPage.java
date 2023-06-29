4b4a969041a648118dfc6204d4ea6acd3604fe58
krishnasaiAsk <138111064+krishnasaiAsk@users.noreply.github.com>
2023-06-29 15:40:23 +0530

    Update LoginPage.java

--- src/main/java/objectRepository/LoginPage.java
+++ src/main/java/objectRepository/LoginPage.java
@@ -6,12 +6,12 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage {
-	System.out.println("code changed from github");
+
 	//declaration
 	@FindBy(xpath="//input[@name='user_name']")private WebElement untxt;
 	@FindBy(xpath="//input[@name='user_password']")private WebElement pwdtxt;
 	@FindBy(xpath="//input[@id='submitButton']")private WebElement loginbtn;
-	
+		System.out.println("code changed from github");
 //initialization
 	
 	public LoginPage(WebDriver driver) {
